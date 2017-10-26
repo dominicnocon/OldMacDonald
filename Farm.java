931abaa8fdefae3b43d704ebe373517fd9160e65
dominicnocon <DoNocon@sfusd.edu>
2017-10-26 13:42:59 -0700

    done

--- Farm.java
+++ Farm.java
@@ -1,14 +1,24 @@
 class Farm 
 {     
-  private Animal[] aBunchOfAnimals = new Animal[3];    
-   public Farm()     {       
+  private Animal[] aBunchOfAnimals = new Animal[4];
+
+	public Farm()     {       
       aBunchOfAnimals[0] = new Cow("cow","moo");           
       aBunchOfAnimals[1] = new Chick("chick", "cluck", "blahblah");       
-      aBunchOfAnimals[2] = new Pig("pig","oink");    }         
+      aBunchOfAnimals[2] = new Pig("pig","oink"); 
+      aBunchOfAnimals[3] = new NamedCow("Bob", "hello");
+        }
+
+
    public void animalSounds()    {       
      for (int nI=0; nI < aBunchOfAnimals.length; nI++)      
       {          
        System.out.println( aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());       
-      }    
+      }   
     } 
 }
+
+
+
+
+
--- NamedCow.java
+++ NamedCow.java
@@ -1,10 +1,10 @@
 class NamedCow extends Cow
 {
 	private String dName;
-	public NamedCow(String type, String sound, String name){
+	public NamedCow(String type, String sound){
 		dType = type;
 		dSound = sound;
-		dName = name;
+		
 	}
  	public String getName(){
  		return dName;
